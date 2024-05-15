a9ba7ffa2f51f41fbba07abbb1bc5d81c8772041
RaeanneZ <raeannezou@gmail.com>
2024-05-15 13:57:46 +0800

    Refactored async code for Update User Password function

--- app/src/main/java/sg/edu/np/mad/mad24p03team2/DatabaseFunctions/UpdateUserPassword.java
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/DatabaseFunctions/UpdateUserPassword.java
@@ -1,58 +1,55 @@
 package sg.edu.np.mad.mad24p03team2.DatabaseFunctions;
 
 import android.content.Context;
-import android.util.Log;
 
 import java.util.ArrayList;
 
-import sg.edu.np.mad.mad24p03team2.AsyncTaskExecutorService.AsyncTaskExecutorService;
 import sg.edu.np.mad.mad24p03team2.Abstract_Interfaces.IDBProcessListener;
+import sg.edu.np.mad.mad24p03team2.AsyncTaskExecutorService.AsyncTaskExecutorService;
 
-public class RegisterUser extends AsyncTaskExecutorService<String, String , String> {
+public class UpdateUserPassword extends AsyncTaskExecutorService<String, String , String>  {
 
     String z = "";
     Boolean isSuccess = false;
     ArrayList<IDBProcessListener> dbListeners = null;
-
     // Login Data Class
     LoginInfoClass loginInfoClass = null;
 
-
-    private ArrayList<IDBProcessListener> listeners = new ArrayList<IDBProcessListener>();
-
-
-    public RegisterUser(Context appContext){
+    public UpdateUserPassword(Context appContext){
+        // Later will pass in ApplicationContext
         this.loginInfoClass = new LoginInfoClass(appContext);
         this.dbListeners = new ArrayList<IDBProcessListener>();
     }
 
-    public RegisterUser(Context appContext, IDBProcessListener listener){
+    public UpdateUserPassword(Context appContext, IDBProcessListener listener){
         this(appContext);
         if(listener != null)
             registerListener(listener);
     }
 
     public void registerListener(IDBProcessListener listener){
-        listeners.add(listener);
+        dbListeners.add(listener);
     }
 
-
     @Override
-    protected String doInBackground(String... inputs)  {
+    protected String doInBackground(String... inputs) {
         try{
             String email = inputs[0];
             String password = inputs[1];
-            isSuccess = loginInfoClass.CreateRecord(email, password);
+            isSuccess = loginInfoClass.UpdateRecord(email, password);
         }
-        catch (Exception e) {
+        catch (Exception e){
+            isSuccess = false;
             z = e.getMessage();
         }
+
         return z;
     }
 
     @Override
     protected void onPostExecute(String s) {
-        for(IDBProcessListener listener : listeners){
+        // Notify all listeners
+        for(IDBProcessListener listener : dbListeners){
             listener.afterProcess(isSuccess);
         }
     }

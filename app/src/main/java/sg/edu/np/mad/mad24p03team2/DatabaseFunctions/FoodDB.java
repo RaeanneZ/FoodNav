b02c033a52f22c760270e62efe24596ffd2b26c6
RaeanneZ <raeannezou@gmail.com>
2024-05-16 22:09:00 +0800

    Created Read Function to Food Database

--- app/src/main/java/sg/edu/np/mad/mad24p03team2/DatabaseFunctions/FoodDB.java
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/DatabaseFunctions/FoodDB.java
@@ -1,4 +1,29 @@
 package sg.edu.np.mad.mad24p03team2.DatabaseFunctions;
 
-public class FoodDB {
+import android.content.Context;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import sg.edu.np.mad.mad24p03team2.Abstract_Interfaces.AbstractDBProcess;
+
+public class FoodDB extends AbstractDBProcess {
+    Statement stmt;
+    protected FoodDB(Context appContext) {
+        super(appContext);
+    }
+
+    // Get Food data through its ID
+    public ResultSet GetRecord(String name, Connection con){
+        String sql = "SELECT * FROM Food WHERE Name = '"+name+"'";
+
+        try {
+            stmt = con.createStatement();
+            return stmt.executeQuery(sql); // Column 1 = email, Column 2 = password
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
 }

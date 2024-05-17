b44d7711337a830fbba822e7fc1b0fc3c4a50fa0
SianKim <siankim06@gmail.com>
2024-05-17 20:46:41 +0800

    finished nutrition info pages, added some comments

--- app/src/main/java/sg/edu/np/mad/mad24p03team2/SelectionActivity3.java
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/SelectionActivity3.java
@@ -10,5 +10,14 @@ public class SelectionActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setup_selectoption_lowsugar2);
+
+        /* MacroView macroView = findViewById(R.id.macro_view);
+        macroView.setRecommendedMacros("Recommended Macros");
+        macroView.setCarbsAmount("23g");
+        macroView.setProteinAmount("23g");
+        macroView.setFatAmount("23g");
+        macroView.setCaloriesAmount("505 Kcal"); */
+
     }
+
 }

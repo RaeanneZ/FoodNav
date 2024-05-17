0bfca7b2db941bd9ee0c0190609525edb5ff9315
SianKim <siankim06@gmail.com>
2024-05-17 11:12:40 +0800

    working buttons and improved layout of pages

---
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/SelectionActivity3.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.mad24p03team2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectionActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup_selectoption_lowsugar2);
+    }
+}

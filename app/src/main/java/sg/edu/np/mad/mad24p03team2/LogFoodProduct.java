c59b9543153ab0da04f5a8db9803d23711a03932
HRx88 <S10262513@connect.np.edu.sg>
2024-05-18 19:35:47 +0800

    dashboard & LogFoodProduct UI done

---
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/LogFoodProduct.java
@@ -0,0 +1,20 @@
+package sg.edu.np.mad.mad24p03team2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LogFoodProduct extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log_food_product, container, false);
+    }
+}

dea30f9233520fe3cadc7c623ee84678e57017c9
SianKim <siankim06@gmail.com>
2024-05-20 18:39:16 +0800

    searchView (design and functionality)

---
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/SearchForFood.java
@@ -0,0 +1,79 @@
+package sg.edu.np.mad.mad24p03team2;
+
+import android.content.ClipData;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchForFood extends Fragment {
+
+    private RecyclerView recyclerView;
+    private List<Item> itemList;
+    private FoodAdapter foodAdapter;
+    private SearchView searchView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_search_for_food, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        searchView = view.findViewById(R.id.searchView);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return true;
+            }
+        });
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+
+        // get from the mssql
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("Fried Bee Hoon, Plain", "252", " cal, 1 plate"));
+        items.add(new Item("Fried Bee Hoon, Plain", "252", " cal, 1 plate"));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new FoodAdapter(getContext(),items));
+    }
+
+    private void filterList(String text) {
+        List<Item> filteredList = new ArrayList<>();
+        for (Item item : itemList) {
+            if (item.getName().toLowerCase().contains(text.toLowerCase())) {
+                filteredList.add(item);
+            }
+        }
+
+        if (filteredList.isEmpty()) {
+            Toast.makeText(getContext(),"No data found", Toast.LENGTH_SHORT).show();
+        } else {
+            foodAdapter.setFilteredList(filteredList);
+        }
+
+    }
+}

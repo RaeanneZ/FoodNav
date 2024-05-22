bff6f2d4478d720177525fac4410d1b517b95aac
SianKim <siankim06@gmail.com>
2024-05-22 13:45:14 +0800

    improved meal slider

--- app/src/main/java/sg/edu/np/mad/mad24p03team2/Dashboard.java
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/Dashboard.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 
 public class Dashboard extends Fragment {
@@ -18,4 +19,6 @@ public class Dashboard extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
+
+
 }

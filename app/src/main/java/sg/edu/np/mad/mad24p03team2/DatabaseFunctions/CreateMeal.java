ece509f5b091ad2c958cc584958ceea572ed768b
RaeanneZ <raeannezou@gmail.com>
2024-05-27 20:24:26 +0800

    Created Function for CreateMeal

---
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/DatabaseFunctions/CreateMeal.java
@@ -0,0 +1,61 @@
+package sg.edu.np.mad.mad24p03team2.DatabaseFunctions;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.util.ArrayList;
+import sg.edu.np.mad.mad24p03team2.Abstract_Interfaces.IDBProcessListener;
+import sg.edu.np.mad.mad24p03team2.AsyncTaskExecutorService.AsyncTaskExecutorService;
+
+public class CreateMeal extends AsyncTaskExecutorService<String, String , String> {
+    Boolean isSuccess = false;
+    ArrayList<IDBProcessListener> dbListeners = null;
+    MealDB mealDB = null;
+
+    public CreateMeal(Context appContext){
+        this.mealDB = new MealDB(appContext);
+        this.dbListeners = new ArrayList<IDBProcessListener>();
+    }
+
+    protected String doInBackground(FoodItemClass foodItem, String... strings) {
+        String mealName = strings[0];
+        String quantity = strings[1];
+        String bloodSugar = strings[2];
+        try {
+            isSuccess = mealDB.CreateRecord(foodItem, mealName, Integer.parseInt(quantity), Float.parseFloat(bloodSugar));
+        } catch (Exception e) {
+            Log.d("Create Meal", "Error occurred: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        for(IDBProcessListener listener : dbListeners){
+            listener.afterProcess(isSuccess);
+        }
+    }
+
+    // IGNORE --------------------------------------------------------------------------------------
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected ArrayList<FoodItemClass> doInBackground() {
+        return null;
+    }
+
+    @Override
+    protected ArrayList<FoodItemClass> doInBackground(String name) {
+        return null;
+    }
+
+    @Override
+    protected DietPlanClass doInBackground(String name, String trackBloodSugar) {
+        return null;
+    }
+    // IGNORE --------------------------------------------------------------------------------------
+}

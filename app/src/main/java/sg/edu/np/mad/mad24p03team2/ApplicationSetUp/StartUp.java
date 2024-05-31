82cc1df574d465ec245e9bb7cc721e68f945a129
RaeanneZ <raeannezou@gmail.com>
2024-05-31 17:34:52 +0800

    Close db connection when app is killed

--- app/src/main/java/sg/edu/np/mad/mad24p03team2/ApplicationSetUp/StartUp.java
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/ApplicationSetUp/StartUp.java
@@ -1,15 +1,23 @@
 package sg.edu.np.mad.mad24p03team2.ApplicationSetUp;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LifecycleObserver;
 
 import java.sql.Connection;
 
 import sg.edu.np.mad.mad24p03team2.DatabaseFunctions.AccountClass;
 import sg.edu.np.mad.mad24p03team2.SingletonSession;
 
-public class StartUp extends Application {
+public class StartUp extends Application implements Application.ActivityLifecycleCallbacks {
     private Connection con = null;
+    private int activityReferences = 0;
+    private boolean isActivityChangingConfigurations = false;
 
     @Override
     public void onCreate() {
@@ -29,5 +37,46 @@ public class StartUp extends Application {
 
     public Connection getConnection(){
         return con;
+    }
+
+    @Override
+    public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(@NonNull Activity activity) {
+
+    }
+
+    // If App goes to the background
+    @Override
+    public void onActivityStopped(@NonNull Activity activity) {
+        
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+    }
+
+    // When App is killed, close connection
+    @Override
+    public void onActivityDestroyed(@NonNull Activity activity) {
+        try {
+            con.close();
+        } catch(Exception e) {
+            Log.d("STARTUP", "Unable to close connection: " + e.getMessage());
+        }
     }
 }

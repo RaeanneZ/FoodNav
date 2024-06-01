e0e35a4a750115881359e906437b21f49d05f4d9
HRx88 <S10262513@connect.np.edu.sg>
2024-06-02 01:42:54 +0800

    finishing function

--- app/src/main/java/sg/edu/np/mad/mad24p03team2/AddFood.java
+++ app/src/main/java/sg/edu/np/mad/mad24p03team2/AddFood.java
@@ -3,12 +3,16 @@ package sg.edu.np.mad.mad24p03team2;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import sg.edu.np.mad.mad24p03team2.Abstract_Interfaces.IDBProcessListener;
+import sg.edu.np.mad.mad24p03team2.DatabaseFunctions.MealClass;
 import sg.edu.np.mad.mad24p03team2.DatabaseFunctions.UpdateMeal;
 import sg.edu.np.mad.mad24p03team2.SingletonClasses.SingletonSession;
 import sg.edu.np.mad.mad24p03team2.SingletonClasses.SingletonTodayMeal;
@@ -17,7 +21,7 @@ import sg.edu.np.mad.mad24p03team2.SingletonClasses.SingletonTodayMeal;
 public class AddFood extends Fragment implements IDBProcessListener {
 
     UpdateMeal updateMeal = null;
-
+private int ns=1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,9 +35,55 @@ public class AddFood extends Fragment implements IDBProcessListener {
         // SingletonTodayMeal.getInstance().AddMeal(mealName);
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_food, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_food, container, false);
+        Button button = view.findViewById(R.id.button2);
+        TextView FoodName=view.findViewById(R.id.textView23);
+        TextView N=view.findViewById(R.id.textView16);
+        TextView nofplate = view.findViewById(R.id.textView26);
+        TextView addn= view.findViewById(R.id.tvadd);
+        TextView Mn= view.findViewById(R.id.textView2);
+
+         addn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 ns++;  // Increment the counter
+                 // Update the TextView with the new value
+                 nofplate.setText(ns+" Plate");
+                 N.setText(ns);
+             }
+         });
+
+        Mn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ns--;  // Increment the counter
+                // Update the TextView with the new value
+                nofplate.setText(ns+" Plate");
+                N.setText(ns);
+            }
+        });
+
+        String mealName  = FoodName.getText().toString();
+        String quantity =N.getText().toString();
+        MealClass mealClass= new MealClass(mealName);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateMeal.execute(Integer.toString(SingletonSession.getInstance().GetAccount().getId()), mealName, quantity);
+                SingletonTodayMeal.getInstance().AddMeal(mealClass);
+                switchFragment();
+            }
+        });
+        return view;
     }
 
+    private void switchFragment() {
+        FragmentActivity activity = getActivity();
+        if (activity instanceof MainActivity2) {
+            ((MainActivity2) activity).replaceFragment(new SearchForFood());
+        }
+    }
     @Override
     public void afterProcess(Boolean executeStatus) {
 
